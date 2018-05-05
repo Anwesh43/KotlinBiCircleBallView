38f641112bf3a3f912edb76d2aca7d8c431bce20
Anwesh43 <anweshthecool0@gmail.com>
2018-05-05 09:36:55 +0530

    adding BiCircleBallView class

--- bicircleballview/src/main/java/com/example/bicircleballview/BiCircleBallView.kt
+++ bicircleballview/src/main/java/com/example/bicircleballview/BiCircleBallView.kt
@@ -7,3 +7,22 @@ package com.example.bicircleballview
 import android.content.*
 import android.graphics.*
 import android.view.*
+
+class BiCircleBallView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+
+}
