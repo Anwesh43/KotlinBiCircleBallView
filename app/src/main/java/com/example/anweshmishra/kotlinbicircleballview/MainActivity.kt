94336b87642ddfa2c883c72f36278701380e52a4
Anwesh43 <anweshthecool0@gmail.com>
2018-05-05 10:19:30 +0530

    adding view in Activity

--- app/src/main/java/com/example/anweshmishra/kotlinbicircleballview/MainActivity.kt
+++ app/src/main/java/com/example/anweshmishra/kotlinbicircleballview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.example.anweshmishra.kotlinbicircleballview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.bicircleballview.BiCircleBallView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BiCircleBallView.render(this)
     }
 }
